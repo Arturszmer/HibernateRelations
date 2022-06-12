fee9fccd3781f72d883ea933461f99b438c74446
ArturSzmer <arturszmer@gmail.com>
2022-06-12 10:20:02 +0200

    first commit

---
+++ src/main/java/com/example/HybernateRelations/HybernateRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HybernateRelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybernateRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybernateRelationsApplication.class, args);
+	}
+
+}
